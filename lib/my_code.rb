068467df29ff8856fb1ba5b102ad4c78f69d92bc
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-24 00:31:05 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,11 +9,15 @@ def map(array)
 end
 
 def reduce(array, starting_point = 0)
-  total = starting_point
-  i = 0
-  while i < array.length 
-   total += yield array[i]
-    i +=1
-  end
+      total = starting_point
+      i = 0
+      while i < array.length
+        if array[i] == false
+          return false
+        end
+        total += array[i]
+        yield(total, array[i])
+        i +=1
+      end
   return total
 end
