97c7608a7211d616464f9b1443a6dafa8293a884
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-24 00:09:16 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(array, starting_point = 0)
   total = starting_point
   i = 0
   while i < array.length 
-    total = yield array[i]
+   total += yield array[i]
     i +=1
   end
   return total
